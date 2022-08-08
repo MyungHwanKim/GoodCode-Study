4738c0700541a2944d2371138e6035d17d82c096
MyungHwanKim <code.kmh9250@gmail.com>
2022-08-08 22:25:14 +0900

    first commit

---
+++ src/main/java/com/example/goodcodestudy/GoodcodeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodcodestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodcodeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodcodeStudyApplication.class, args);
+    }
+
+}
