73bccbf8df6f8bc9bfb5eb04979c5b4d494be791
MyungHwanKim <code.kmh9250@gmail.com>
2022-08-10 11:29:41 +0900

    first commit

---
+++ src/test/java/com/example/goodcodestudy/GoodcodeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodcodestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodcodeStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
